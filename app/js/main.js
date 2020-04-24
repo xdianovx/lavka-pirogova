83d585deb2cd688383e945f29916e4bb8b11d6c6
Dianov Evgenii <justsmm@mail.ru>
2020-04-24 13:51:03 +0300

    Плюс простые блоки - Десктоп

--- app/js/main.js
+++ app/js/main.js
@@ -8,5 +8,10 @@ $(document).ready(function () {
         prevArrow: '<button id="prev" type="button" class="btn slick-custom-prev"></button>',
         nextArrow: '<button id="next" type="button" class="btn slick-custom-next"></button>'
     });
+
+    $('.franchisee__slider').slick({
+        prevArrow: '<button id="prev" type="button" class="btn slick-custom-prev"></button>',
+        nextArrow: '<button id="next" type="button" class="btn slick-custom-next"></button>'
+    });
 });
 
