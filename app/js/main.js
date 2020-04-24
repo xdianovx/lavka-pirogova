7c8f0a20e234e14db5eb757c028843954ade5311
Dianov Evgenii <justsmm@mail.ru>
2020-04-24 11:20:14 +0300

    unique Десктоп

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,6 @@
-$(function(){
-
-
-
-  
+$(document).ready(function () {
+    $('.unique__slider').slick({
+        prevArrow: '<button id="prev" type="button" class="btn slick-custom-prev"></button>',
+        nextArrow: '<button id="next" type="button" class="btn slick-custom-next"></button>'
+    });
 });
