0991425ec36a21c91aede727d38449a378ecd3b2
Dianov Evgenii <justsmm@mail.ru>
2020-04-24 12:33:15 +0300

    Formats - десктоп

--- app/js/main.js
+++ app/js/main.js
@@ -3,4 +3,10 @@ $(document).ready(function () {
         prevArrow: '<button id="prev" type="button" class="btn slick-custom-prev"></button>',
         nextArrow: '<button id="next" type="button" class="btn slick-custom-next"></button>'
     });
+
+    $('.formats__slider').slick({
+        prevArrow: '<button id="prev" type="button" class="btn slick-custom-prev"></button>',
+        nextArrow: '<button id="next" type="button" class="btn slick-custom-next"></button>'
+    });
 });
+
