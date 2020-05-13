65033ed89b5971db50c0022c9a579d69ae0d8f14
Dianov Evgenii <justsmm@mail.ru>
2020-05-13 11:05:59 +0300

    Якоря + адаптив

--- app/js/main.js
+++ app/js/main.js
@@ -113,4 +113,12 @@ $(function () {
     });
 });
 
+$(document).ready(function(){
+    $('a[href^="#"], *[data-href^="#"]').on('click', function(e){
+        e.preventDefault();
+        var t = 1000;
+        var d = $(this).attr('data-href') ? $(this).attr('data-href') : $(this).attr('href');
+        $('html,body').stop().animate({ scrollTop: $(d).offset().top -250}, t);
 
+    });
+});
